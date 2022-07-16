f546d5c971a24238211bbe67b1b3eb26b3b9d9aa
Abushama Siddique <abushamasiddique175@gmail.com>
2022-07-16 20:27:47 +0530

    Assignment Done

--- solution.js
+++ solution.js
@@ -5,11 +5,11 @@ function major(arr)
   var count = [];
   var max;
   for(var i=0;i<n;i++){
-    count[arr[i]]++;
+    count[arr[i]] = count[arr[i]] + 1;
     if(count[arr[i]] > floor(n/2)){
       max = arr[i];
     }
   }
   return max;
-}
+}add 
 module.exports=major;
